d888e13ad0ac98a060824b9673a309dcf59ed61b
koojunho <koojh@9cells.com>
2016-03-19 05:50:31 +0900

    매 분마다 스킨 바꾸기

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use Artisan;
+use Storage;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -19,12 +21,35 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () use ($schedule) {
+            $this->rrmdir(resource_path('views/vendor/ncells'));
+            $provider = array_rand([
+                'ModernPUG\OriginalSkin\OriginalSkinServiceProvider',
+                'ModernPUG\RedGooseSkin\RedGooseSkinServiceProvider',
+            ]);
+            Artisan::call('vendor:publish', ['--provider' => $provider]);
+        })->everyMinute();
+    }
+
+    private function rrmdir($dirPath)
+    {
+        $paths = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(
+                $dirPath,
+                \FilesystemIterator::SKIP_DOTS
+            ),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($paths as $path) {
+            $path->isDir() && !$path->isLink() ? rmdir($path->getPathname()) : unlink($path->getPathname());
+        }
+        
+        rmdir($dirPath);
     }
 }
