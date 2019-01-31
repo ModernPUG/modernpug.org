27fb541c5bbdc88c1c7ed154d5e8f2ee3fd7f1dc
kkame <kkamenet@gmail.com>
2019-01-31 13:17:09 +0900

    테스트용 배너 제거 및 임시 배너 추가

--- resources/views/widget/banner.blade.php
+++ resources/views/widget/banner.blade.php
@@ -4,10 +4,9 @@
     <h5 class="title">Banner</h5>
     <div class="widget-content">
         <!-- Single Blog Post -->
-        <div class="single-blog-post post-style-2 d-flex align-items-center widget-post">
-            <a href="http://log.op.gg/op-gg-recruit-wanted-web-developer-2018/" target="_blank">
-                <img src="//laravel.kr/assets/images/banners/opgg-banner.png">
-            </a>
-        </div>
+        <a class="single-blog-post post-style-2 d-flex align-items-center widget-post"
+           href="http://www.jobkorea.co.kr/Recruit/GI_Read/27349638" target="_blank">
+            <img src="/img/recruit/airtel_banner.png">
+        </a>
     </div>
 </div>
