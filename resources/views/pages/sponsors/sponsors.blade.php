c41fc7b1705e041273cf97178c8a1431777ff8b7
Hyunseok Lee <smartbosslee@gmail.com>
2019-02-02 06:48:08 +0900

    스폰서 이미지와 링크 추가

--- resources/views/pages/sponsors/sponsors.blade.php
+++ resources/views/pages/sponsors/sponsors.blade.php
@@ -39,6 +39,38 @@
                     <div class="row">
 
                         <div class="col-md-4">
+                            <a href="http://imicorp.co.kr/" target="_blank" class="headline">
+                                <!-- Single Blog Post -->
+                                <div class="single-blog-post">
+                                    <!-- Post Thumbnail -->
+                                    <div class="post-thumbnail">
+                                        <img src="/img/sponsors/imi.jpg" alt="">
+                                    </div>
+                                    <!-- Post Content -->
+                                    <div class="post-content">
+                                        <h5>(주)아이엠아이</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-md-4">
+                            <a href="https://www.xpressengine.com/" target="_blank" class="headline">
+                                <!-- Single Blog Post -->
+                                <div class="single-blog-post">
+                                    <!-- Post Thumbnail -->
+                                    <div class="post-thumbnail">
+                                        <img src="/img/sponsors/xe.jpg" alt="">
+                                    </div>
+                                    <!-- Post Content -->
+                                    <div class="post-content">
+                                        <h5>Xpress Engine</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-md-4">
                             <a href="https://www.jetbrains.com/" target="_blank" class="headline">
                                 <!-- Single Blog Post -->
                                 <div class="single-blog-post">
@@ -53,6 +85,7 @@
                                 </div>
                             </a>
                         </div>
+
                     </div>
 
                 </div>
--- resources/views/widget/sponsor.blade.php
+++ resources/views/widget/sponsor.blade.php
@@ -4,6 +4,11 @@
     <h5 class="title">Sponsor</h5>
     <div class="widget-content">
         <div class="single-blog-post post-style-2 d-flex align-items-center widget-post">
+            <a href="http://imicorp.co.kr/" target="_blank">
+                <img src="/img/sponsors/imi_banner.jpg" alt="(주)아이엠아이">
+            </a>
+        </div>
+        <div class="single-blog-post post-style-2 d-flex align-items-center widget-post">
             <a href="https://www.xpressengine.com/" target="_blank">
                 <img src="/img/sponsors/XE_banner_type1.jpg" alt="XpressEngine">
             </a>
