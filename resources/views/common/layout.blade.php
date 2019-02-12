b3f3d43fe8a54213e87a17b4e26ad1912a3d00f5
kkame <kkamenet@gmail.com>
2019-02-12 22:08:07 +0900

    opensearch 추가

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -19,6 +19,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link type="application/opensearchdescription+xml" rel="search" href="{{ asset('opensearch.xml') }}"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="@yield('meta.description',config('website.meta.description',''))"/>
     <meta name="keywords" content="@yield('meta.keywords',config('website.meta.keywords',''))"/>
