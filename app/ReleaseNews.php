85c8571e73c76c40ce7c9949940ea8c28e19148e
Mingeun Kim <mingeun.k.k@gmail.com>
2019-02-22 01:54:17 +0900

    릴리즈 모든 타입 버전 크롤링 리메이크

--- app/ReleaseNews.php
+++ app/ReleaseNews.php
@@ -32,40 +32,55 @@ class ReleaseNews extends Model
 {
     const SUPPORT_RELEASES = [
         'PHP' => [
-            'site_url'  => 'http://php.net/ChangeLog-7.php',
-            'version'   => '#layout-content > section > h3',
-            'content'   => '#layout-content > section > ul',
-            'date'      => '#layout-content > section > b > time'
-        ],
-        'Laravel' => [
-            'site_url'  => 'https://github.com/laravel/laravel/releases',
-            'version'   => 'body > div.application-main > div > div > div.container-lg.new-discussion-timeline.experiment-repo-nav.p-responsive > div.repository-content > div.position-relative.border-top.clearfix > div:nth-child(1) > div > div.d-none.d-md-block.flex-wrap.flex-items-center.col-12.col-md-3.col-lg-2.px-md-3.pb-1.pb-md-4.pt-md-4.float-left.text-md-right.v-align-top > ul > li:nth-child(1) > a > span', // fixed laravel release version child(3)
-            'content'   => 'body > div.application-main > div > div > div.container-lg.new-discussion-timeline.experiment-repo-nav.p-responsive > div.repository-content > div.position-relative.border-top.clearfix > div:nth-child(1) > div > div.col-12.col-md-9.col-lg-10.px-md-3.py-md-4.release-main-section.commit.open.float-left > div.markdown-body',
-            'date'      => 'body > div.application-main > div > div > div.container-lg.new-discussion-timeline.experiment-repo-nav.p-responsive > div.repository-content > div.position-relative.border-top.clearfix > div:nth-child(1) > div > div.col-12.col-md-9.col-lg-10.px-md-3.py-md-4.release-main-section.commit.open.float-left > div.release-header > p > relative-time'
-        ],
-        'Codeigniter' => [
-            'site_url'  => 'https://www.codeigniter.com/userguide3/changelog.html',
-            'version'   => '#change-log > div > h2',
-            'content'   => '#change-log > div',
-            'date'      => '#change-log > div > p'
-        ],
-        'Symfony' => [
-            'site_url'  => 'https://symfony.com/blog/category/releases',
-            'version'   => '#content_wrapper > div > div.row > main > div > div > h2 > a',
-            'content'   => '#content_wrapper > div > div.row > main > section > .post__content',
-            'date'      => '#content_wrapper > div > div.row > main > div > div > p.metadata > span',
-            'post'   =>  [ // if 2depth release content
-                'url'       => 'https://symfony.com/blog/',
-                'before'    => '/[. ]/',
-                'after'     => '-',
+            'site_url'  => 'http://php.net/releases/',
+            'version'   => '#layout-content > h2',
+            'date'      => '#layout-content > ul > li:nth-child(1)',
+            'post'      => [
+                'url'       => 'http://php.net/releases/#',
+                'before'    => '',
+                'after'     => '',
+                'end'       => ''
             ]
-        ]
+        ],
+         'Laravel' => [
+             'site_url'  => 'https://github.com/laravel/laravel/releases',
+             'version'   => 'body > div.application-main > div > div > div.container-lg.new-discussion-timeline.experiment-repo-nav.p-responsive > div.repository-content > div.position-relative.border-top.clearfix > div > div > div.d-none.d-md-block.flex-wrap.flex-items-center.col-12.col-md-3.col-lg-2.px-md-3.pb-1.pb-md-4.pt-md-4.float-left.text-md-right.v-align-top > ul > li > a > span', // fixed laravel release version child(3)
+             'date'      => 'body > div.application-main > div > div > div.container-lg.new-discussion-timeline.experiment-repo-nav.p-responsive > div.repository-content > div.position-relative.border-top.clearfix > div > div > div.col-12.col-md-9.col-lg-10.px-md-3.py-md-4.release-main-section.commit.open.float-left > div.release-header > p > relative-time',
+             'post'      => [
+                 'url'      => 'https://github.com/laravel/laravel/releases/tag/v',
+                 'before'   => '',
+                 'after'    => '',
+                 'end'      => ''
+             ]
+         ],
+         'Codeigniter' => [
+             'site_url'  => 'https://www.codeigniter.com/userguide3/changelog.html',
+             'version'   => '#change-log > div > h2',
+             'date'      => '#change-log > div > p',
+             'post'     => [
+                 'url'      => 'https://www.codeigniter.com/userguide3/changelog.html#version-',
+                 'before'   => '/[. ]/',
+                 'after'    => '-',
+                 'end'      => ''
+             ]
+         ],
+         'Symfony' => [
+             'site_url'  => 'https://symfony.com/blog/category/releases',
+             'version'   => '#content_wrapper > div > div.row > main > div > div > h2 > a',
+             'date'      => '#content_wrapper > div > div.row > main > div > div > p.metadata > span',
+             'post'   =>  [
+                 'url'       => 'https://symfony.com/blog/symfony-',
+                 'before'    => '/[. ]/',
+                 'after'     => '-',
+                 'end'      => '-released'
+             ]
+         ]
     ];
 
     protected $table = 'release_news';
 
     protected $fillable = [
-        'site_url', 'type', 'version', 'content', 'released_at'
+        'site_url', 'type', 'version', 'released_at'
     ];
 
     /**
--- app/Services/ReleaseNews/Updater.php
+++ app/Services/ReleaseNews/Updater.php
@@ -36,40 +36,49 @@ class Updater {
      * @throws \Exception
      */
     public function update(int $success = 0, int $duplicate = 0, int $fail = 0) {
-        $datas = $this->releaseNews::SUPPORT_RELEASES;
-        $this->print('릴리즈 크롤링 데이터 검색 건수: ' . count($datas));
+        $this->print('크롤링 시작');
 
-        foreach ($datas as $type => $data) {
+        foreach ($this->releaseNews::SUPPORT_RELEASES as $type => $data) {
             try {
                 DB::beginTransaction();
 
                 $crawler = $this->convertCrawlerFromUrl($data['site_url']);
-                $version = $crawler->filter($data['version'])->text();
-                $releasedAt = $this->releaseDateModify(trim($crawler->filter($data['date'])->text()));
 
-                if (isset($data['post'])) {
-                    $siteUrl = $this->releaseInContent($data['post']['url'],
-                        $data['post']['before'],
-                        $data['post']['after'],
-                        $version);
-                    $crawler = $this->convertCrawlerFromUrl($siteUrl);
+                $version = $crawler->filter($data['version'])->each(function ($content) {
+                    return $content->text();
+                });
+
+                $releasedAt = $crawler->filter($data['date'])->each(function ($content) {
+                    return $content->text();
+                });
+
+                $releaseArray = $this->mergeCrawlerResult($version, $releasedAt);
+
+                foreach ($releaseArray as $release) {
+                    if ($this->releaseNews::existTypeAndVersion($type, $this->releaseVersionCheck($release[0]))) {
+                        $duplicate++;
+                        continue;
+                    }
+
+                    $this->print($type . '=> ' . $release[0]);
+
+                    if (isset($data['post']['url'])) {
+                        $siteUrl = $this->releaseInContent($data['post']['url'],
+                            $data['post']['before'],
+                            $data['post']['after'],
+                            $this->releaseVersionCheck($release[0]),
+                            $data['post']['end']);
+                    }
+
+                    $this->releaseNews::create([
+                        'site_url' => empty($data['post']['url']) ? $data['site_url'] : $siteUrl,
+                        'type' => $type,
+                        'version' => $this->releaseVersionCheck($release[0]),
+                        'released_at' => $this->releaseDateModify($release[1]),
+                    ]);
+
+                    $success++;
                 }
-
-                $content = $crawler->filter($data['content'])->text();
-
-                if ($this->releaseNews::existTypeAndVersion($type, $this->releaseVersionCheck($version))) {
-                    $duplicate++;
-                    continue;
-                }
-
-                $this->releaseNews::create([
-                    'site_url' => isset($data['post']) ? $siteUrl : $data['site_url'],
-                    'type' => $type,
-                    'version' => $this->releaseVersionCheck($version),
-                    'content' => $content,
-                    'released_at' => $releasedAt,
-                ]);
-                $success++;
 
                 DB::commit();
             } catch (\Exception $e) {
@@ -97,6 +106,21 @@ class Updater {
     }
 
     /**
+     * @param mixed ...$argv
+     * @return array
+     */
+    private function mergeCrawlerResult(...$argv) {
+        $result = [];
+        foreach ($argv as $array) {
+            foreach ($array as $index => $value) {
+                $result[$index][] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
      * @param   string $version
      * @return  string
      */
@@ -109,10 +133,15 @@ class Updater {
      * @param   string $before
      * @param   string $after
      * @param   string $version
+     * @param string $end
      * @return  string
      */
-    private function releaseInContent(string $url, string $before, string $after, string $version) {
-        return $url . strtolower(preg_replace($before, $after, trim($version)));
+    private function releaseInContent(string $url, string $before, string $after, string $version, string $end) {
+        if (empty($before) && empty($after)) {
+            return $url . $version . $end;
+        } else {
+            return $url . strtolower(preg_replace($before, $after, trim($version))) . $end;
+        }
     }
 
     /**
@@ -121,6 +150,9 @@ class Updater {
      */
     private function releaseDateModify(string $date) {
         // TODO: CI 경우 릴리즈 날짜에 문자가 포함 되어있으므로 제외시켜야함.
+        if (strpos($date, ':') !== false) {
+            return date('y-m-d', strtotime(substr($date, strpos($date, ':') + 2)));
+        }
         return date('y-m-d', strtotime($date));
     }
 
