e9a34b0ee9f0e08b1e468b89b1362426f31fee86
kkame <kkamenet@gmail.com>
2019-04-18 20:06:54 +0900

    브리치 채용공고 등록 및 배너 등록
    - 에어텔닷컴 채용공고 제거

--- resources/views/pages/recruit/index.blade.php
+++ resources/views/pages/recruit/index.blade.php
@@ -32,27 +32,27 @@
                 <div class="col-md-4">
                     <!-- Single Blog Post -->
                     <div class="single-blog-post recruit">
-                        <a href="http://www.jobkorea.co.kr/Recruit/GI_Read/27349638" target="_blank" class="headline">
+                        <a href="https://recruit.brich.co.kr/" target="_blank" class="headline">
                             <!-- Post Thumbnail -->
                             <div class="post-thumbnail">
-                                <img src="/img/recruit/rgb_airtel_logo.png" alt="에어텔닷컴 백엔드 개발자 채용" style="padding:80px 30px;">
+                                <img src="/img/recruit/brich_logo.jpg" alt="에어텔닷컴 백엔드 개발자 채용" style="padding:80px 30px;">
                                 <!-- category -->
-                                <div class="label">상시채용</div>
+                                <div class="label">Laravel</div>
                             </div>
                             <!-- Post Content -->
                             <div class="post-content">
-                                <h5>신입/경력 PHP백엔드 개발자 채용</h5>
+                                <h5>워라벨, 라라벨 좋아하는 Modern PHP 개발자 모집</h5>
                                 <p>
-                                    에어텔닷컴
+                                    브리치
                                 </p>
                                 <p>
-                                    에어텔닷컴은 자유여행 전문 여행사입니다.
-                                    PHP를 주력으로 사용하고 있으며 항공권,호텔,티켓,렌트카 등
-                                    해외여행과 관련된 시스템 개발을 합니다.
+                                    Laravel 과 Vue.js 로 개발한 최신 커머스 시스템을 경험해보세요.
+                                    브리치 개발팀에 합류하면 그간의 노하우를 전수 받으실 수 있고,
+                                    새로운 프로젝트들을 진행하면서 재미있게 개발할 수 있다고 기대하셔도 좋습니다.
                                 </p>
                                 <!-- Post Meta -->
                                 <address>
-                                    서울 중구
+                                    서울 강남구 봉은사로
                                 </address>
                             </div>
                         </a>
--- resources/views/widget/banner.blade.php
+++ resources/views/widget/banner.blade.php
@@ -3,10 +3,11 @@
 <div class="sidebar-widget-area banner">
     <h5 class="title">Banner</h5>
     <div class="widget-content">
-        <!-- Single Blog Post -->
+
         <a class="single-blog-post post-style-2 d-flex align-items-center widget-post"
-           href="http://www.jobkorea.co.kr/Recruit/GI_Read/27349638" target="_blank">
-            <img src="/img/recruit/airtel_banner.png">
+           href="https://recruit.brich.co.kr/" target="_blank">
+            <img src="/img/recruit/brich_banner.jpg">
         </a>
+
     </div>
 </div>
