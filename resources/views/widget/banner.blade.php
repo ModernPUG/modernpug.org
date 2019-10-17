9d1c176214d26a99a81d1791f9650cbcec916119
kkame <kkamenet@gmail.com>
2019-10-17 21:11:39 +0900

    팀오투, 에어텔닷컴 채용공고 배너 추가

--- resources/views/widget/banner.blade.php
+++ resources/views/widget/banner.blade.php
@@ -9,5 +9,18 @@
             <img src="/img/recruit/brich_banner.jpg">
         </a>
 
+
+        <a class="single-blog-post post-style-2 d-flex align-items-center widget-post"
+           href="http://www.jobkorea.co.kr/Recruit/GI_Read/29823090?Oem_Code=C1" target="_blank">
+            <img src="/img/recruit/airtel_banner.png">
+        </a>
+
+
+        <a class="single-blog-post post-style-2 d-flex align-items-center widget-post"
+           href="http://www.saramin.co.kr/zf_user/jobs/view?rec_idx=37009515&view_type=etc" target="_blank">
+            <img src="/img/recruit/carmore-1.png">
+        </a>
+
+
     </div>
 </div>
