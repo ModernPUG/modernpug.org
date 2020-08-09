8bb8811828d4b528390a96ab270f21c0c96a2eba
kkame <kkamenet@gmail.com>
2020-08-09 23:28:29 +0900

    채용정보 api / 지난 채용정보 노출 제거.

--- app/Http/Controllers/Api/V1/RecruitController.php
+++ app/Http/Controllers/Api/V1/RecruitController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Recruit;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class RecruitController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
-        return Recruit::collection(\App\Recruit::paginate());
+        return Recruit::collection(\App\Recruit::where('expired_at', '>=', Carbon::now())->paginate());
     }
 
     /**
@@ -31,7 +33,7 @@ class RecruitController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -42,7 +44,7 @@ class RecruitController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param int $id
+     * @param  int  $id
      * @return Recruit
      */
     public function show($id)
@@ -53,7 +55,7 @@ class RecruitController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -64,8 +66,8 @@ class RecruitController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -76,7 +78,7 @@ class RecruitController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param int $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
