d40fe5c6a013c08cf228b0b967fbaa1b29199101
kkame <kkamenet@gmail.com>
2020-11-05 22:31:48 +0900

    유니크 처리 추가.

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\Blog::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
-        'feed_url' => $faker->url,
-        'site_url' => $faker->url,
+        'feed_url' => $faker->unique()->url,
+        'site_url' => $faker->unique()->url,
         'description' => $faker->paragraph,
         'image_url' => $faker->url,
         'owner_id' => factory(\App\Models\User::class),
