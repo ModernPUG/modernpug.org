bcaf5bca775accc5ed3b83f9b44bcb8144390d29
kkame <kkamenet@gmail.com>
2020-11-23 00:22:18 +0900

    게시글 조회 api 태그 필터 추가.

--- app/Http/Controllers/Api/V1/Posts/IndexController.php
+++ app/Http/Controllers/Api/V1/Posts/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Posts\IndexRequest;
 use App\Http\Resources\Post as PostResource;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Eloquent\Builder;
 
 class IndexController extends Controller
@@ -15,6 +16,7 @@ class IndexController extends Controller
         $createdAt = $request->input('created_at');
         $createdFrom = $request->input('created_from');
         $createdTo = $request->input('created_to');
+        $tagNames = Tag::getAllManagedTags();
 
         $posts = Post::when($createdAt, function (Builder $builder) use ($createdAt) {
             $builder->whereBetween('created_at', [$createdAt.' 00:00:00', $createdAt.' 23:59:59']);
@@ -22,6 +24,8 @@ class IndexController extends Controller
             $builder->where('created_at', '>=', $createdFrom.' 00:00:00');
         })->when($createdTo, function (Builder $builder) use ($createdTo) {
             $builder->where('created_at', '<=', $createdTo.' 23:59:59');
+        })->whereHas('tags', function (Builder $builder) use ($tagNames) {
+            $builder->whereIn('tags.name', $tagNames);
         })->get();
 
         return PostResource::collection($posts);
--- tests/Feature/Api/V1/Posts/IndexTest.php
+++ tests/Feature/Api/V1/Posts/IndexTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Api\V1\Posts;
 
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -22,8 +23,17 @@ class IndexTest extends TestCase
     public function testGetAllPostsWithAuthorization()
     {
         $yesterday = Carbon::yesterday();
-        factory(Post::class)->create(['created_at' => $yesterday]);
-        factory(Post::class)->create();
+        /**
+         * @var Post $yesterdayPost
+         */
+        $yesterdayPost = factory(Post::class)->create(['created_at' => $yesterday]);
+        $yesterdayPost->tags()->sync(Tag::firstOrCreate(['name' => Tag::MANAGED_TAGS['PHP'][0]]));
+
+        /**
+         * @var Post $post
+         */
+        $post = factory(Post::class)->create();
+        $post->tags()->sync(Tag::firstOrCreate(['name' => Tag::MANAGED_TAGS['PHP'][0]]));
 
         /**
          * @var User $user
@@ -59,8 +69,19 @@ class IndexTest extends TestCase
     {
         $yesterday = Carbon::yesterday();
         $today = Carbon::today();
-        factory(Post::class)->create(['created_at' => $yesterday]);
-        factory(Post::class)->create(['created_at' => $today]);
+
+        /**
+         * @var Post $yesterdayPost
+         */
+        $yesterdayPost = factory(Post::class)->create(['created_at' => $yesterday]);
+        $yesterdayPost->tags()->sync(Tag::firstOrCreate(['name' => Tag::MANAGED_TAGS['PHP'][0]]));
+
+        /**
+         * @var Post $todayPost
+         */
+        $todayPost = factory(Post::class)->create(['created_at' => $today]);
+        $todayPost->tags()->sync(Tag::firstOrCreate(['name' => Tag::MANAGED_TAGS['PHP'][0]]));
+
 
         /**
          * @var User $user
@@ -97,8 +118,17 @@ class IndexTest extends TestCase
     {
         $yesterday = Carbon::yesterday();
         $today = Carbon::today();
-        factory(Post::class)->create(['created_at' => $yesterday]);
-        factory(Post::class)->create(['created_at' => $today]);
+        /**
+         * @var Post $yesterdayPost
+         */
+        $yesterdayPost = factory(Post::class)->create(['created_at' => $yesterday]);
+        $yesterdayPost->tags()->sync(Tag::firstOrCreate(['name' => Tag::MANAGED_TAGS['PHP'][0]]));
+
+        /**
+         * @var Post $todayPost
+         */
+        $todayPost = factory(Post::class)->create(['created_at' => $today]);
+        $todayPost->tags()->sync(Tag::firstOrCreate(['name' => Tag::MANAGED_TAGS['PHP'][0]]));
 
         /**
          * @var User $user
@@ -129,5 +159,47 @@ class IndexTest extends TestCase
         ])
             ->assertJsonCount('1', 'data')
             ->assertJsonPath('data.0.created_at', $yesterday->toJSON());
+    }
+
+    public function testCheckFilterTags()
+    {
+        /**
+         * @var Post $trackedTagPost
+         */
+        $trackedTagPost = factory(Post::class)->create();
+        $trackedTagPost->tags()->sync(Tag::firstOrCreate(['name' => Tag::MANAGED_TAGS['PHP'][0]]));
+
+        /**
+         * @var Post $untrackedTagPost
+         */
+        $untrackedTagPost = factory(Post::class)->create();
+        $untrackedTagPost->tags()->sync(Tag::firstOrCreate(['name' => 'UNTRACKED_TAG']));
+
+        /**
+         * @var User $user
+         */
+        $user = factory(User::class)->create();
+
+        $token = $user->createToken('test token', ['*'])->accessToken;
+
+        $response = $this->getJson(route('api.v1.posts.index', [
+        ]), ['Authorization' => 'Bearer '.$token]);
+
+        $response->assertOk()->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'link',
+                    'origin_link',
+                    'description',
+                    'blog_id',
+                    'published_at',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ])
+            ->assertJsonCount('1', 'data');
     }
 }
