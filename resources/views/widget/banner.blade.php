ec3aa66ff033dc0af0f8d79d644fb27f9a203a84
kkame <kkamenet@gmail.com>
2022-04-24 02:03:31 +0900

    배너 섹션 노출 기준 수정.

--- resources/views/widget/banner.blade.php
+++ resources/views/widget/banner.blade.php
@@ -4,19 +4,21 @@
      */
 @endphp
 
-<!-- Widget Area -->
-<div class="sidebar-widget-area banner">
-    <h5 class="title">Recruit</h5>
-    <div class="widget-content">
+@if(count($banners))
+    <!-- Widget Area -->
+    <div class="sidebar-widget-area banner">
+        <h5 class="title">Recruit</h5>
+        <div class="widget-content">
 
-        @foreach($banners as $banner)
-            <a class="single-blog-post post-style-2 d-flex align-items-center widget-post" href="{{ $banner->url }}"
-               target="_blank">
-                <img
-                    src="{{ asset($banner->images()->first()->file_path??"/img/adult-article-assortment-1496183.jpg") }}"
-                    title="{{ $banner->title }}">
-            </a>
-        @endforeach
+            @foreach($banners as $banner)
+                <a class="single-blog-post post-style-2 d-flex align-items-center widget-post" href="{{ $banner->url }}"
+                   target="_blank">
+                    <img
+                        src="{{ asset($banner->images()->first()->file_path??"/img/adult-article-assortment-1496183.jpg") }}"
+                        title="{{ $banner->title }}">
+                </a>
+            @endforeach
 
+        </div>
     </div>
-</div>
+@endif
