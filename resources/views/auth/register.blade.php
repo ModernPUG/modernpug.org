a02ff993251a9d41ac410d84df3c6fe0c77a9425
kkame <kkamenet@gmail.com>
2022-04-23 21:25:55 +0900

    회원가입 소셜 링크 수정.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -100,11 +100,25 @@
                             <div class="form-group">
                                 <div class="col-md-6 offset-md-4">
 
-                                    <a class="btn btn-outline-secondary btn-block" href="{{ route('login.social',['slack']) }}">
-                                        <strong>
-                                            <i class="fa fa-slack icon"></i> Login with Slack
-                                        </strong>
-                                    </a>
+                                    @if(config('services.slack.client_id'))
+
+                                        <a class="btn btn-outline-secondary btn-block"
+                                           href="{{ route('login.social',['slack']) }}">
+                                            <strong>
+                                                <i class="fa fa-slack icon"></i> Login with Slack
+                                            </strong>
+                                        </a>
+                                    @endif
+
+                                    @if(config('services.discord.client_id'))
+
+                                        <a class="btn btn-outline-secondary btn-block"
+                                           href="{{ route('login.social',['discord']) }}">
+                                            <strong>
+                                                <i class="fa fa-discord icon"></i> Login with Discord
+                                            </strong>
+                                        </a>
+                                    @endif
                                 </div>
                             </div>
 
